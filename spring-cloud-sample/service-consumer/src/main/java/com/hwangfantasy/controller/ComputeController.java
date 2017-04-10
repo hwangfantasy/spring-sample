b9d58991a7f07e0c0bbcb84f4429914e7f448bf6
hwangfantasy <fan.huang@ideacome.com>
2017-04-10 10:46:54 +0800

    改造使用spring cloud config

--- spring-cloud-sample/service-consumer/src/main/java/com/hwangfantasy/controller/ComputeController.java
+++ spring-cloud-sample/service-consumer/src/main/java/com/hwangfantasy/controller/ComputeController.java
@@ -1,24 +1,33 @@
 package com.hwangfantasy.controller;
 
-import com.hwangfantasy.service.ComputeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hwangfantasy.service.ComputeService;
 
 /**
  * @作者 hwangfantasy
  * @创建时间: 2017/4/10 <br/>
  * @方法描述: 样例计算服务消费. <br/>
  */
+@RefreshScope
 @RestController
 @RequestMapping(value = "/compute")
 public class ComputeController {
-
+    @Value("${name}")
+    private String name;
     @Autowired
     ComputeService computeService;
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
         return computeService.addService();
+    }
+    @RequestMapping(value = "sayHi")
+    public String sayHi(){
+        return this.name;
     }
 }
--- spring-cloud-sample/service-provider/src/main/java/com/hwangfantasy/controller/ComputeController.java
+++ spring-cloud-sample/service-provider/src/main/java/com/hwangfantasy/controller/ComputeController.java
@@ -1,5 +1,7 @@
 package com.hwangfantasy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @创建时间: 2017/4/10 <br/>
  * @方法描述: 样例计算服务. <br/>
  */
-
+@RefreshScope
 @RestController
 @RequestMapping(value = "/compute")
 public class ComputeController {
-
+    @Value("${name}")
+    private String name;
     @RequestMapping(value = "/add",method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a,@RequestParam Integer b){
         return a+b;
+    }
+
+    @RequestMapping(value = "/sayHi")
+    public String sayHi(){
+        return this.name;
     }
 }
