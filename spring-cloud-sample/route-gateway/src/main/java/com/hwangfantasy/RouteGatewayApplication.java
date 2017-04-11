8b01265f6b83f432534cdc7b1671cf02931cf0a2
hwangfantasy <fan.huang@ideacome.com>
2017-04-11 09:42:48 +0800

    服务过滤

--- spring-cloud-sample/route-gateway/src/main/java/com/hwangfantasy/RouteGatewayApplication.java
+++ spring-cloud-sample/route-gateway/src/main/java/com/hwangfantasy/RouteGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.hwangfantasy;
 
+import com.hwangfantasy.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -10,5 +12,10 @@ public class RouteGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RouteGatewayApplication.class, args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
 	}
 }
