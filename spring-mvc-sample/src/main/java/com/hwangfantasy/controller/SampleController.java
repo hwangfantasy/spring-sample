5fac06109b5b4b34237c0a8ea6fa9d2b5eb61518
hwangfantasy <fan.huang@ideacome.com>
2017-04-13 11:04:53 +0800

    add mvc mysql

--- spring-mvc-sample/src/main/java/com/hwangfantasy/controller/SampleController.java
+++ spring-mvc-sample/src/main/java/com/hwangfantasy/controller/SampleController.java
@@ -1,7 +1,11 @@
 package com.hwangfantasy.controller;
 
+import com.hwangfantasy.bean.User;
+import com.hwangfantasy.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -12,10 +16,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/sample")
 public class SampleController {
+    @Autowired
+    private SampleService sampleService;
 
     @RequestMapping("/sayHi")
     @ResponseBody
     public String sayHi(){
         return "Hello Fantasy!";
+    }
+
+    @RequestMapping("/addUser")
+    @ResponseBody
+    public String addUser(@RequestParam String name,@RequestParam String password,@RequestParam String phone){
+        User user = new User();
+        sampleService.addUser(user.withName(name).withPassword(password).withPhone(phone));
+        return "success";
     }
 }
--- spring-mvc-sample/src/main/java/com/hwangfantasy/service/SampleService.java
+++ spring-mvc-sample/src/main/java/com/hwangfantasy/service/SampleService.java
@@ -1,10 +1,21 @@
 package com.hwangfantasy.service;
 
+import com.hwangfantasy.bean.User;
+import com.hwangfantasy.dao.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @作者 yunfeiyang
  * @创建时间: 2017/4/12 <br/>
  * @方法描述: TODO ADD FUNCTION. <br/>
  */
-
+@Service
 public class SampleService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public void addUser(User user){
+        userMapper.insert(user);
+    }
 }
