0de4324a3251e7857c4f3806c0aeba667b5cc242
hwangfantasy <fan.huang@ideacome.com>
2017-05-08 15:28:50 +0800

    mvc mysql主从库配置

---
+++ spring-mvc-sample/src/main/java/com/hwangfantasy/aop/DynamicDataSource.java
@@ -0,0 +1,10 @@
+package com.hwangfantasy.aop;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DataSourceSwitcher.getDataSource();
+    }
+}
