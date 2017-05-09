ecabe855e0e9d49dbf3aefb4c643b7b91ea4173e
hwangfantasy <fan.huang@ideacome.com>
2017-05-09 15:42:04 +0800

    annotation

--- spring-mvc-sample/src/main/java/com/hwangfantasy/annotation/SampleControllerAnnotation.java
+++ spring-mvc-sample/src/main/java/com/hwangfantasy/annotation/SampleControllerAnnotation.java
@@ -1,5 +1,6 @@
 package com.hwangfantasy.annotation;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.lang.annotation.*;
@@ -13,6 +14,7 @@ import java.lang.annotation.*;
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @RequestMapping("/sample")
+@Controller
 @Documented
 public @interface SampleControllerAnnotation {
     String name() default "";
