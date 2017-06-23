731fde0fa77467de480a64ebc0f1416b7f71cb58
hwangfantasy <fan.huang@ideacome.com>
2017-06-23 11:15:47 +0800

    boot logback email

--- spring-boot-sample/src/main/java/com/hwangfantasy/SpringBootSampleApplication.java
+++ spring-boot-sample/src/main/java/com/hwangfantasy/SpringBootSampleApplication.java
@@ -1,12 +1,17 @@
 package com.hwangfantasy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MarkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.hwangfantasy")
 public class SpringBootSampleApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootSampleApplication.class);
 	public static void main(String[] args) {
+		LOGGER.error(MarkerFactory.getMarker("NOTIFY_ADMIN"),"邮件测试");
+
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
 }
